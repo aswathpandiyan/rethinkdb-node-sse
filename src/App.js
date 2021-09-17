39b0a06e9e5187d57749ba84b864d595e5239213
aswathpandiyan <aswathpandiyan@gmail.com>
2021-09-17 19:20:38 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './style.css';
 import React, { useState, useEffect } from 'react';
 
 const useEventSource = (url) => {
-  const [data, updateData] = useState(null);
+  const [data, updateData] = useState([]);
 
   useEffect(() => {
     const source = new EventSource(url);
 
     source.onmessage = function logEvents(event) {
-      updateData(JSON.parse(event.data));
+      updateData([...data, JSON.parse(event.data)]);
     };
   }, []);
 
@@ -16,14 +16,15 @@ const useEventSource = (url) => {
 };
 
 export default function App() {
-  const data = useEventSource('https://ds.shub.dev/e/temperatures');
-  if (!data) {
-    return <div />;
+  const data = useEventSource('http://localhost:5000/errors');
+  if (data.length <= 0) {
+    return <div>No data found</div>;
   }
   return (
     <div>
-      The current temperature in my living room is {data.temperature} as of{' '}
-      {data.updatedAt}
+      {data.map((row, index) => {
+        return <div key={index}>{row.new_value}</div>;
+      })}
     </div>
   );
 }
